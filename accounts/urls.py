adae78d389fbb06dcaf3919e08e857098f26d75e
IMAD EL GHADAOUI <imad.elghadaoui@uit.ac.ma>
2024-04-19 11:30:27 +0100

    Add homepage

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 # accounts/urls.py
 from django.urls import path
-from .views import register
+from .views import register, homepage
 from .views import user_login, user_logout
 
 urlpatterns = [
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
     path('register/', register, name='register'),
+    path('home/', homepage, name='homepage'),
+   
 ]
-
--- accounts/views.py
+++ accounts/views.py
@@ -22,13 +22,11 @@ def user_login(request):
 
 # accounts/views.py
 from django.contrib.auth import logout
-from django.shortcuts import redirect
 
 def user_logout(request):
     logout(request)
     return redirect('login')  # Redirect to the login page after logout
 
-from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
@@ -40,4 +38,8 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'registration.html', {'form': form})
+
+def homepage(request):
+    return render(request, 'homepage.html')
+
 
