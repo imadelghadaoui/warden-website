490485519b29f024e1e98842e5111327c86b371a
imad24kn <imad03marrake@gmail.com>
2024-04-29 16:26:24 +0100

    dashboard

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class StudentClass(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+class Classes(models.Model):
+    name=models.CharField(max_length=50)
+    nombre=models.IntegerField(default=0)
+    id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
